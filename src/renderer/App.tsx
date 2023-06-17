3188dddd16620a05bf4589d6d5975150b70689fa
Matheus Diniz <mtsdnz@hotmail.com>
2023-06-17 15:59:44 -0300

    Remove react router

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,22 +3,9 @@ import React from 'react';
 import './dependencyInjection';
 import { create, Workbench } from 'mo';
 import './mo-lib/style/mo.scss';
-import extensions from './extensions';
 
-const moInstance = create({
-  extensions,
-});
-
-function Hello() {
-  return moInstance.render(<Workbench />);
-}
+const moInstance = create({ extensions: [] });
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+  return moInstance.render(<Workbench />);
 }
