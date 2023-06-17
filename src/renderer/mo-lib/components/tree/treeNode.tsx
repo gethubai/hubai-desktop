ca23c8064c7c68f13e06c4877c90de80bbe86162
Matheus Diniz <mtsdnz@hotmail.com>
2023-06-17 15:57:24 -0300

    small fixes

--- src/renderer/mo-lib/components/tree/treeNode.tsx
+++ src/renderer/mo-lib/components/tree/treeNode.tsx
@@ -39,89 +39,89 @@ interface ITreeNodeProps {
 }
 const INDENT = 8;
 
-export default function({
-    data,
-    indent,
-    className,
-    name,
-    renderIcon,
-    renderTitle,
-    draggable,
-    onContextMenu,
-    onClick,
-    onNodeDragStart,
-    onNodeDragEnter,
-    onNodeDragOver,
-    onNodeDrop,
-    onNodeDragEnd,
+export default function ({
+  data,
+  indent,
+  className,
+  name,
+  renderIcon,
+  renderTitle,
+  draggable,
+  onContextMenu,
+  onClick,
+  onNodeDragStart,
+  onNodeDragEnter,
+  onNodeDragOver,
+  onNodeDrop,
+  onNodeDragEnd,
 }: ITreeNodeProps) {
-    const uuid = data.id;
-    const ref = useRef<HTMLDivElement>(null);
+  const uuid = data.id;
+  const ref = useRef<HTMLDivElement>(null);
 
-    const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-        e.stopPropagation();
-        onNodeDragStart?.(e, data);
-        try {
-            // ie throw error
-            // firefox-need-it
-            e.dataTransfer.setData('text/plain', '');
-        } catch (error) {
-            // empty
-        }
-    };
+  const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    onNodeDragStart?.(e, data);
+    try {
+      // ie throw error
+      // firefox-need-it
+      e.dataTransfer.setData('text/plain', '');
+    } catch (error) {
+      // empty
+    }
+  };
 
-    const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        onNodeDragEnter?.(e, data);
-    };
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onNodeDragEnter?.(e, data);
+  };
 
-    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        onNodeDragOver?.(e, data);
-    };
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onNodeDragOver?.(e, data);
+  };
 
-    const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        onNodeDrop?.(e, data);
-    };
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onNodeDrop?.(e, data);
+  };
 
-    const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-        e.stopPropagation();
-        onNodeDragEnd?.(e, data);
-    };
+  const handleDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    onNodeDragEnd?.(e, data);
+  };
 
-    // calculate key automatically via parent path and self id
-    const nodeKey = `${indent ? indent + '_' : ''}${data.id}`;
+  // calculate key automatically via parent path and self id
+  const nodeKey = `${indent ? `${indent}_` : ''}${data.id}`;
 
-    return (
-        <div
-            ref={ref}
-            key={`${uuid}-${indent}`}
-            tabIndex={0}
-            data-indent={indent}
-            data-key={uuid}
-            data-id={`mo_treeNode_${nodeKey}`}
-            className={className}
-            title=
-            draggable={draggable}
-            onContextMenu={onContextMenu}
-            onClick={onClick}
-            onDragStart={handleDragStart}
-            onDragOver={handleDragOver}
-            onDragEnter={handleDragEnter}
-            onDragEnd={handleDragEnd}
-            onDrop={handleDrop}
-        >
-            <div className={indentClassName} style={{ width: INDENT * indent }}>
-                {new Array(indent).fill('').map((_, index) => (
-                    <div key={index} className={indentGuideClassName} />
-                ))}
-            </div>
-            {renderIcon()}
-            <span className={treeNodeTitleClassName}>{renderTitle()}</span>
-        </div>
-    );
-};
+  return (
+    <div
+      ref={ref}
+      key={`${uuid}-${indent}`}
+      tabIndex={0}
+      data-indent={indent}
+      data-key={uuid}
+      data-id={`mo_treeNode_${nodeKey}`}
+      className={className}
+      title={name}
+      draggable={draggable}
+      onContextMenu={onContextMenu}
+      onClick={onClick}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnter={handleDragEnter}
+      onDragEnd={handleDragEnd}
+      onDrop={handleDrop}
+    >
+      <div className={indentClassName} style={{ width: INDENT * indent }}>
+        {new Array(indent).fill('').map((_, index) => (
+          <div key={index} className={indentGuideClassName} />
+        ))}
+      </div>
+      {renderIcon()}
+      <span className={treeNodeTitleClassName}>{renderTitle()}</span>
+    </div>
+  );
+}
