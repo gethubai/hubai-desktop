ac4af97a3e8f6148b95bb52f50c0f424a038cc64
Matheus Diniz <mtsdnz@hotmail.com>
2023-06-23 09:13:11 -0300

    Change appDataStorage path to use the userData path instead of appData

--- src/utils/pathUtils.ts
+++ src/utils/pathUtils.ts
@@ -1,11 +1,22 @@
 import { app } from 'electron';
 import path from 'path';
+import fs from 'fs';
 
 export const getAppDataStoragePath = (append: string) =>
-  path.normalize(`${app.getPath('appData')}/allai-app/${append}`);
+  path.normalize(`${app.getPath('userData')}/${append}`);
 
 export const getMessageStoragePath = (append: string) =>
-  getAppDataStoragePath(`messages/${append}`);
+  getAppDataStoragePath(`chat/messages/${append}`);
 
 export const getMessageAudioStoragePath = (append: string) =>
   getMessageStoragePath(`audio/${append}`);
+
+export const createDirectoryIfNotExists = (directoryPath: string) => {
+  try {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  } catch (err: any) {
+    if (err.code !== 'EEXIST') {
+      throw err;
+    }
+  }
+};
