ffef2bf55df8df1b7a0fee7060d0299ea2ada37d
Matheus Diniz <mtsdnz@hotmail.com>
2023-07-04 12:08:15 -0300

    Delete database implementation for RxDB on brain

--- src/data/brain/db-main.ts
+++
@@ -1,6 +0,0 @@
-import { Database, internalGetDatabase } from './db';
-import getStorage from '../storage';
-
-export function getBrainDatabase(): Promise<Database> {
-  return internalGetDatabase(getStorage());
-}
