5280d3c873133d49de5ccefb6619befcc1f55bc0
Matheus Diniz <mtsdnz@hotmail.com>
2023-07-08 16:14:00 -0300

    Use SettingsMapForm component on LocalBrainWindow

--- src/renderer/features/brain/workbench/localBrainWindow.tsx
+++ src/renderer/features/brain/workbench/localBrainWindow.tsx
@@ -1,44 +1,10 @@
-import validator from '@rjsf/validator-ajv8';
-import Form from '@rjsf/core';
-import { RJSFSchema } from '@rjsf/utils';
-import { LocalBrainSettingMap } from 'api-server/brain/domain/models/localBrain';
+import SettingsMapForm from 'renderer/components/form/settingsMapForm';
 import { IBrainController } from '../controllers/type';
 import { IBrainState, LocalBrainViewModel } from '../models/brain';
 
 export interface ILocalBrainWindowProps extends IBrainController, IBrainState {
   brain: LocalBrainViewModel;
   getCurrentSettings: () => any;
-}
-
-function buildProperties(settingsMap: LocalBrainSettingMap[]) {
-  const required: string[] = [];
-
-  if (!settingsMap) return;
-
-  const properties = {} as any;
-
-  settingsMap.forEach((setting) => {
-    const { name, displayName, type, defaultValue, enumValues, description } =
-      setting;
-    const property = {
-      type,
-      title: displayName,
-      default: defaultValue,
-      enum: enumValues && enumValues.length > 0 ? enumValues : undefined,
-      description,
-      originalName: name,
-    };
-
-    const keyName = name; // name.split('.').pop();
-
-    if (setting.required) {
-      required.push(keyName);
-    }
-
-    properties[keyName] = property;
-  });
-
-  return { properties, required };
 }
 
 function LocalBrainWindow({
@@ -48,24 +14,12 @@ function LocalBrainWindow({
 }: ILocalBrainWindowProps) {
   const { settingsMap } = brain;
 
-  let schema: RJSFSchema;
-
   const showSettings = !!settingsMap && settingsMap.length > 0;
-
-  if (showSettings) {
-    const schemaResult = buildProperties(settingsMap);
-    schema = {
-      title: 'Brain Settings',
-      type: 'object',
-      required: schemaResult?.required,
-      properties: schemaResult?.properties,
-    };
-  }
 
   const currentSettings = getCurrentSettings();
 
-  const onSubmit = (e) => {
-    onSaveSettings?.(brain, e.formData);
+  const onSubmit = (formData: any) => {
+    onSaveSettings?.(brain, formData);
   };
 
   return (
@@ -77,17 +31,15 @@ function LocalBrainWindow({
 
       {showSettings && (
         <div id="settings_container" style={{ marginTop: 20 }}>
-          <Form
-            schema={schema}
-            formData={currentSettings}
-            validator={validator}
+          <SettingsMapForm
+            settingsMap={settingsMap}
+            currentSettings={currentSettings}
             onSubmit={onSubmit}
-            noValidate
           >
             <div>
               <button type="submit">Save Settings</button>
             </div>
-          </Form>
+          </SettingsMapForm>
         </div>
       )}
     </div>
