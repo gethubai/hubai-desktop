1ea156afbb743f01ad58e20eab94777a2f068d55
Matheus Diniz <mtsdnz@hotmail.com>
2023-07-10 13:59:57 -0300

    Add msg scheme to allow getting msg data from fetch api

--- src/main/main.ts
+++ src/main/main.ts
@@ -32,6 +32,13 @@ import 'api-server/extensions/ipc/mainApi';
 const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
+protocol.registerSchemesAsPrivileged([
+  {
+    scheme: 'msg',
+    privileges: { supportFetchAPI: true, stream: true },
+  },
+]);
+
 if (!isDebug) {
   console.log = log.log;
   Object.assign(console, log.functions);
