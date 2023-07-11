e01871f5c4eb610b981e16338ef759eea99eaf37
Matheus Diniz <mtsdnz@hotmail.com>
2023-07-11 13:00:07 -0300

    accept undefined dictionary

--- src/data/realm/helpers.ts
+++ src/data/realm/helpers.ts
@@ -1,6 +1,8 @@
 import Realm from 'realm';
 
-export const dictionaryToObject = (dictionary: Realm.Dictionary<any>) => {
+export const dictionaryToObject = (
+  dictionary: Realm.Dictionary<any> | undefined
+) => {
   if (!dictionary) return dictionary;
 
   return Object.entries(dictionary).reduce((obj, [key, value]) => {
