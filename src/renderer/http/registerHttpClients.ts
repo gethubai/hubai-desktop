22c2fd1127b7ac050e3079209fbea1da298ddb79
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-10 13:49:10 -0300

    register package and download httpClients

---
+++ src/renderer/http/registerHttpClients.ts
@@ -0,0 +1,30 @@
+import { container } from 'tsyringe';
+import {
+  BuiltInHttpClient,
+  HttpClientFactory,
+  type IHttpClientFactory,
+} from './httpClientFactory';
+import { AxiosHttpClient } from './axios/axiosHttpClient';
+
+container.registerSingleton<IHttpClientFactory>(
+  'IHttpClientFactory',
+  HttpClientFactory
+);
+
+const httpClientFactory =
+  container.resolve<IHttpClientFactory>('IHttpClientFactory');
+
+// TODO: Get from env
+const apiBaseUrl = 'https://localhost:7216/api';
+
+httpClientFactory.addHttpClient(
+  BuiltInHttpClient.packageApi,
+  new AxiosHttpClient({
+    baseURL: `${apiBaseUrl}/package`,
+  })
+);
+
+httpClientFactory.addHttpClient(
+  BuiltInHttpClient.downloadApi,
+  new AxiosHttpClient({ baseURL: `${apiBaseUrl}/download` })
+);
