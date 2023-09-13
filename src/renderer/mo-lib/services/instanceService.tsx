5c1e371872afa3b351d85179dfa37823af59b2f4
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-13 17:11:47 -0300

    Disable search sidebar

--- src/renderer/mo-lib/services/instanceService.tsx
+++ src/renderer/mo-lib/services/instanceService.tsx
@@ -86,7 +86,7 @@ export default class InstanceService
         'IOutlineController',
         'IPanelController',
         'IProblemsController',
-        'ISearchController',
+        //   'ISearchController',
         'ISettingsController',
         'ISidebarController',
         'IStatusBarController',
