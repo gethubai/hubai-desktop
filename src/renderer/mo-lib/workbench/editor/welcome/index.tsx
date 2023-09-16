8215bef0f87b53fb039e5cae9b1930773342dfdd
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-16 14:21:27 -0300

    Add HubAI logo to welcome screen (yes, now we have a logo!)

--- src/renderer/mo-lib/workbench/editor/welcome/index.tsx
+++ src/renderer/mo-lib/workbench/editor/welcome/index.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import { prefixClaName } from '@hubai/core/esm/common/className';
 // import { loadComponent } from 'renderer/common/dynamicModule';
 import useGetKeys from './hooks';
+import Logo from './logo';
 
 export default function Welcome() {
   const keys = useGetKeys();
 
   return (
     <div className={prefixClaName('welcome')}>
-      {/* <Logo className="logo" /> */}
-      <h1 className="title">HubAI</h1>
+      <Logo className="logo" />
       <div className="keybindings">
         <ul>
           {keys.map((item) => {
