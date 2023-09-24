2a3c985b61062bdb6837757499ce550885487e68
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-24 13:58:08 -0300

    remove uuid package and use built-in crypto.randoomUUId instead

--- src/renderer/common/uniqueIdGenerator.ts
+++ src/renderer/common/uniqueIdGenerator.ts
@@ -1,5 +1,27 @@
-import { v4 as uuidv4 } from 'uuid';
+/* eslint-disable eqeqeq */
+/* eslint-disable no-bitwise */
+
+// NodeJS doesn't have the crypto.randomUUID() so we do it manually
+function manualUUID(): string {
+  let d = new Date().getTime();
+  let d2 =
+    (typeof performance !== 'undefined' &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    let r = Math.random() * 16;
+    if (d > 0) {
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c == 'x' ? r : (r & 0x7) | 0x8).toString(16);
+  });
+}
 
 export default function generateUniqueId(): string {
-  return uuidv4();
+  return typeof crypto === 'undefined' ? manualUUID() : crypto.randomUUID();
 }
