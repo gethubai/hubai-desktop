e444ea7a2fedcf06036d37b3711390f68471417f
Matheus Diniz <mtsdnz@hotmail.com>
2023-09-24 14:21:35 -0300

    Show a warning instead of an error when a symlink already exists

--- .erb/scripts/link-modules.ts
+++ .erb/scripts/link-modules.ts
@@ -4,6 +4,18 @@ import webpackPaths from '../configs/webpack.paths';
 const { srcNodeModulesPath } = webpackPaths;
 const { appNodeModulesPath } = webpackPaths;
 
-if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
-  fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
+try {
+  if (!fs.existsSync(srcNodeModulesPath) && fs.existsSync(appNodeModulesPath)) {
+    fs.symlinkSync(appNodeModulesPath, srcNodeModulesPath, 'junction');
+  }
+} catch (error: any) {
+  if (error.code === 'EEXIST') {
+    console.warn(
+      `A file or directory already exists at ${srcNodeModulesPath}.`
+    );
+    // Optionally, remove the existing file/directory or decide what action to take.
+  } else {
+    // If it's a different error, rethrow it.
+    throw error;
+  }
 }
