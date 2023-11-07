e3072ee7bcc9ff101b626a527f3a0189a68e60f1
Matheus Diniz <mtsdnz@hotmail.com>
2023-11-07 13:59:49 -0300

    apply realm fix on every platform

--- .erb/scripts/fix-realm.js
+++ .erb/scripts/fix-realm.js
@@ -4,14 +4,14 @@ import { execSync } from 'child_process';
 import webpackPaths from '../configs/webpack.paths';
 import path from 'path';
 
-if (process.platform === 'win32') {
-	const realmPath = path.join(webpackPaths.appNodeModulesPath, 'realm');
-	
-	console.log("Applying Realm fix on path: " + realmPath);
-	execSync('npx prebuild-install --runtime napi', {
-    cwd: realmPath,
-    stdio: 'inherit',
-  });
+//if (process.platform === 'win32') {
+const realmPath = path.join(webpackPaths.appNodeModulesPath, 'realm');
 
-  console.log("Realm fix has been applied!");
-}
+console.log('Applying Realm fix on path: ' + realmPath);
+execSync('npx prebuild-install --runtime napi', {
+  cwd: realmPath,
+  stdio: 'inherit',
+});
+
+console.log('Realm fix has been applied!');
+//}
